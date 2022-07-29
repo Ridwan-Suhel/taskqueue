c22fe1084277f78267e7ebb89c6fee911e9e79ca
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-07-29 21:09:27 +0600

    all product data fetched and showing dynamically from server

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleProducts from "./SingleProducts";
 
 const Products = () => {
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProductData(data));
+  }, []);
+
   return (
-    <main className="px-4 pt-4">
+    <main className="px-4 pt-4 bg-slate-50">
       <div className="title">
         <h2 className="text-2xl">Chose your best product</h2>
         <p className="text-base">
@@ -12,11 +20,9 @@ const Products = () => {
       </div>
 
       <div className="product-wrapper grid lg:grid-cols-3 2xl:grid-cols-4 gap-2 pt-12">
-        {/* single col  */}
-        <SingleProducts />
-        <SingleProducts />
-        <SingleProducts />
-        <SingleProducts />
+        {productData.map((item) => (
+          <SingleProducts item={item} key={item._id} />
+        ))}
       </div>
     </main>
   );
--- src/pages/Products/SingleProducts.js
+++ src/pages/Products/SingleProducts.js
@@ -14,32 +14,33 @@ import speaker from "../../images/speaker.png";
 import usb from "../../images/usb.png";
 import "./Products.css";
 
-const SingleProducts = () => {
+const SingleProducts = ({ item }) => {
+  const { name, price, model, image } = item;
   return (
-    <div className="overflow-hidden group single-col border rounded-lg px-6 py-4 cursor-pointer relative">
+    <div className="bg-white overflow-hidden group single-col border rounded-lg px-6 py-4 cursor-pointer relative">
       <div className="flex justify-between items-center">
-        <div className="pr-name font-bold text-base">Vanity chair red</div>
+        <div className="pr-name font-bold text-base">{name}</div>
         <span>
           <HeartIcon className="w-5 h-5 text-rose-500" />
         </span>
       </div>
       <div className="img-wrapper py-3">
-        <img className="w-[200px] mx-auto" src={chair} alt="img" />
+        <img className="w-[200px] mx-auto" src={image} alt="img" />
       </div>
 
       <div className="flex justify-between items-center">
         <div className="pr-model font-bold text-sm flex gap-1 items-center">
           <ShieldExclamationIcon className="w-5 h-5 text-slate-900" />
-          <h3>Office chair CSM 220-12</h3>
+          <h3>{model}</h3>
         </div>
-        <p className="font-bold text-sm">$120</p>
+        <p className="font-bold text-sm">${price}</p>
       </div>
 
       <div className="transition-all hover-back w-full h-[80px] bg-black absolute bottom-[-100%] group-hover:bottom-0 left-0">
         <div className="w-full pt-4 px-4 text-white text-lg absolute top-0 left-[-100%] group-hover:left-0 transition-all delay-300">
-          <h2 className="leading-[16px]">Product Name</h2>
+          <h2 className="leading-[16px]">{name}</h2>
           <div className="flex items-center justify-between ">
-            <h2 className="text-base">Price $220</h2>
+            <h2 className="text-base">Price: ${price}</h2>
             <button className="buy-btn relative flex text-sm items-center px-3 py-1 border-2 border-white rounded-lg text-white">
               <span className="btn-icon z-10 bg-black absolute translate-y-[-50%] top-[50%] left-[-20px] border-2 p-2 rounded-full">
                 <ShoppingCartIcon className="w-4 h-4 text-white " />
