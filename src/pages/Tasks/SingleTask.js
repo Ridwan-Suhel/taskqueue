3a7c256bdbd980a8a4245e08bdf94a3b7e182390
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-08-01 19:02:31 +0600

    showing specific todos data using custom modal

--- src/pages/Tasks/SingleTask.js
+++ src/pages/Tasks/SingleTask.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { HeartIcon } from "@heroicons/react/outline";
 import { InformationCircleIcon } from "@heroicons/react/solid";
+import TaskDetails from "./TaskDetails";
 const SingleTask = ({ todo }) => {
+  const [handleClick, setHandleClick] = useState(false);
+
   const {
     description,
     email,
@@ -14,35 +17,47 @@ const SingleTask = ({ todo }) => {
     taskfor,
   } = todo;
   return (
-    <div className="relative cursor-pointer flex justify-between gap-4 border border-slate-400 bg-gray-300 mb-2 px-4 py-3 rounded">
-      <div className="flex gap-4 w-full">
-        <div className="flex gap-4 mt-1 w-[80px]">
-          <input
-            type="checkbox"
-            className="border-slate-900 rounded h-5 w-5 cursor-pointer"
-          />
-          <button className="heart-btn h-6 w-6">
-            <HeartIcon className="h-6 w-6 text-slate-900 mt-[-4px]" />
-          </button>
-        </div>
+    <div>
+      <div className="relative cursor-pointer flex justify-between gap-4 border border-slate-400 bg-gray-300 mb-2 px-4 rounded">
+        <div className="flex gap-4 w-full">
+          <div className="flex gap-4 mt-1 w-[80px]  py-3">
+            <input
+              type="checkbox"
+              className="border-slate-900 rounded h-5 w-5 cursor-pointer"
+            />
+            <button className="heart-btn h-6 w-6">
+              <HeartIcon className="h-6 w-6 text-slate-900 mt-[-4px]" />
+            </button>
+          </div>
 
-        <div className="group relative w-full">
-          <h2 className="text-lg">{title}</h2>
-          <div className="pointer-events-none  z-[10] opacity-0  group-hover:opacity-90 group-hover:visible transition-all duration-500 rounded-lg arrow-content absolute translate-y-[-40%] bottom-[100%] left-[0px] bg-blue-500 border border-blue-600">
-            <p className="text-sm text-white py-2 px-4">
-              Click for view details
-            </p>
-            <div class="absolute top-full left-[10%] h-0 w-0 border-x-8 border-x-transparent border-t-[16px] border-t-blue-500"></div>
+          <div
+            onClick={() => {
+              setHandleClick(!handleClick);
+            }}
+            className="group relative w-full py-3"
+          >
+            <h2 className="text-lg">{title}</h2>
+            <div className="pointer-events-none  z-[10] opacity-0  group-hover:opacity-90 group-hover:visible transition-all duration-500 rounded-lg arrow-content absolute translate-y-[-40%] bottom-[100%] left-[0px] bg-blue-500 border border-blue-600">
+              <p className="text-sm text-white py-2 px-4">
+                Click for view details
+              </p>
+              <div class="absolute top-full left-[10%] h-0 w-0 border-x-8 border-x-transparent border-t-[16px] border-t-blue-500"></div>
+            </div>
+          </div>
+        </div>
+        {/* right side  */}
+        <div className="w-[150px]  py-3">
+          <div className="flex gap-2 items-center justify-end">
+            <p className="text-lg text-slate-900 font-medium">{todosEndDate}</p>
+            <InformationCircleIcon className="h-7 w-7 text-gray-500 mt-1 cursor-pointer" />
           </div>
         </div>
       </div>
-      {/* right side  */}
-      <div className="w-[150px] ">
-        <div className="flex gap-2 items-center justify-end">
-          <p className="text-lg text-slate-900 font-medium">{todosEndDate}</p>
-          <InformationCircleIcon className="h-7 w-7 text-gray-500 mt-1 cursor-pointer" />
-        </div>
-      </div>
+      <TaskDetails
+        todo={todo}
+        handleClick={handleClick}
+        setHandleClick={setHandleClick}
+      />
     </div>
   );
 };
---
+++ src/pages/Tasks/TaskDetails.js
@@ -0,0 +1,65 @@
+import { XIcon } from "@heroicons/react/solid";
+import React from "react";
+
+const TaskDetails = ({ todo, handleClick, setHandleClick }) => {
+  console.log(handleClick);
+
+  const {
+    description,
+    email,
+    name,
+    title,
+    userPhoto,
+    todosCreateDate,
+    todosEndDate,
+    _id,
+    taskfor,
+  } = todo;
+  return (
+    <div
+      className={`${
+        handleClick ? "opacity-100 visible" : "opacity-0 invisible"
+      } transition-all duration-500 fixed top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] flex items-center justify-center bg-slate-900 w-full h-screen z-20`}
+    >
+      <div className="relative w-3/5 bg-blue-100 py-6 px-8 rounded-lg">
+        <div>
+          <div>
+            <h2 className="text-xl font-[500]">
+              Your Task Title: <span className="font-[400]">{title}</span>
+            </h2>
+            <h2 className="text-xl font-[500]">
+              Task For: <span className="font-[400]">{taskfor}</span>
+            </h2>
+            <h2 className="text-xl font-[500] mt-4">
+              Task Description:{" "}
+              <span className="font-[400]">{description}</span>
+            </h2>
+          </div>
+          <div className="flex gap-3 items-center mt-4">
+            <div>
+              <span className="text-sm font-[500]">Create Date</span>
+              <p className="text-right text-xl bg-green-300 px-5 py-1 rounded-md">
+                {todosCreateDate}
+              </p>
+            </div>
+            <div>
+              <span className="text-sm font-[500]">Planning to end</span>
+              <p className="text-right text-xl bg-red-300 px-5 py-1 rounded-md">
+                {todosEndDate}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <button
+          onClick={() => setHandleClick(!handleClick)}
+          className="absolute top-[15px] right-[15px] hover:scale-105 transition-all bg-slate-900 w-8 h-8 rounded-full"
+        >
+          <XIcon className="w-5 h-5 text-white mx-auto" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TaskDetails;
